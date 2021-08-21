478e9216fe461b020a4b217fc06027f93e443b05
Jake Cyr <jake.cyr@oliveai.com>
2021-08-20 22:56:45 -0400

    Revert property name changes

--- source/TreeScopeWrapper.cc
+++ source/TreeScopeWrapper.cc
@@ -10,13 +10,13 @@ NAN_MODULE_INIT(TreeScopeWrapper::Init)
 
     Local<Object> treeScope = Object::New(isolate);
 
-    Nan::Set(treeScope, Nan::New("None").ToLocalChecked(), Nan::New(TreeScope_None));
-    Nan::Set(treeScope, Nan::New("Element").ToLocalChecked(), Nan::New(TreeScope_Element));
-    Nan::Set(treeScope, Nan::New("Children").ToLocalChecked(), Nan::New(TreeScope_Children));
-    Nan::Set(treeScope, Nan::New("Descendants").ToLocalChecked(), Nan::New(TreeScope_Descendants));
-    Nan::Set(treeScope, Nan::New("Parent").ToLocalChecked(), Nan::New(TreeScope_Parent));
-    Nan::Set(treeScope, Nan::New("Ancestors").ToLocalChecked(), Nan::New(TreeScope_Ancestors));
-    Nan::Set(treeScope, Nan::New("Subtree").ToLocalChecked(), Nan::New(TreeScope_Subtree));
+    Nan::Set(treeScope, Nan::New("TreeScope_None").ToLocalChecked(), Nan::New(TreeScope_None));
+    Nan::Set(treeScope, Nan::New("TreeScope_Element").ToLocalChecked(), Nan::New(TreeScope_Element));
+    Nan::Set(treeScope, Nan::New("TreeScope_Children").ToLocalChecked(), Nan::New(TreeScope_Children));
+    Nan::Set(treeScope, Nan::New("TreeScope_Descendants").ToLocalChecked(), Nan::New(TreeScope_Descendants));
+    Nan::Set(treeScope, Nan::New("TreeScope_Parent").ToLocalChecked(), Nan::New(TreeScope_Parent));
+    Nan::Set(treeScope, Nan::New("TreeScope_Ancestors").ToLocalChecked(), Nan::New(TreeScope_Ancestors));
+    Nan::Set(treeScope, Nan::New("TreeScope_Subtree").ToLocalChecked(), Nan::New(TreeScope_Subtree));
 
     Nan::Set(target, Nan::New("TreeScope").ToLocalChecked(), treeScope);
 }
